cc7285e8724a7f7df39d1e1a9aa5bb26d37e277c
ttonya <s.tonya0805@gmail.com>
2018-07-09 22:47:11 +0700

    new repository

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+
+const config = {
+
+  entry: './src/app.js',
+
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+     new HtmlWebpackPlugin({
+    title: 'CV',
+    template: './src/index.pug'
+  }),
+  new MiniCssExtractPlugin({
+    filename: 'main.css'
+  })
+   ],
+
+  module:{
+    rules: [
+      {
+               test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+               use: [{
+                   loader: 'file-loader',
+                   options: {
+                       name: '[name].[ext]',
+                       outputPath: 'fonts/'
+                   }
+               }]
+           },
+        {
+          test: /\.styl$/,
+          use:[ MiniCssExtractPlugin.loader,  'css-loader',  'stylus-loader']
+        },
+      {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              use: [ 'babel-loader' ]
+            },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+             },
+      {
+        test: /\.(pug|jade)$/,
+        loader: 'pug-loader',
+        options: {
+      pretty: true
+    }
+  }
+]
+}
+}
+module.exports = config;
